c8d124d304ccf3b71abf74a1463a7cf0710e55b6
ambidextrousTx <ravisinha@my.unt.edu>
2016-04-08 21:36:02 -0500

    Add stub for the starting point

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+// This is the starting point
+//
+#include <iostream>
+
+int main() {
+    std::string testString("This is a sample string");
+    std::cout << testString << std::endl;
+
+    return 0;
+}
