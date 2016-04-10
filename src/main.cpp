c88d59009ed01886ede5a93330009b63fe5151a0
ambidextrousTx <ravisinha@my.unt.edu>
2016-04-10 01:23:07 -0500

    Implement basic string matching

--- src/main.cpp
+++ src/main.cpp
@@ -5,10 +5,18 @@
 int main() {
     std::string greeting("Welcome to RGrep-CPP");
     std::cout << greeting << std::endl;
-    std::string inputString;
+    std::string firstInputString;
+    std::string secondInputString;
 
-    std::cin >> inputString;
-    std::cout << "You are looking for " << inputString << std::endl;
+    std::cout << "Enter the first string" << std::endl;
+    std::cin >> firstInputString;
+    std::cout << "Enter the second string" << std::endl;
+    std::cin >> secondInputString;
+
+    if (firstInputString == secondInputString) {
+        std::cout << "Match!" << std::endl;
+
+    }
 
     return 0;
 }
