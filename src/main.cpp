ce8aaf3b9ff437e73ad7c9e17adb3a9e3f56756a
ambidextrousTx <ravisinha@my.unt.edu>
2016-04-12 01:52:56 -0500

    Add funtionality to match in files

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 // This is the starting point
 //
 #include <iostream>
+#include <fstream>
+#include <sstream>
 
 int main() {
     std::string greeting("Welcome to RGrep-CPP");
@@ -15,7 +17,29 @@ int main() {
 
     if (firstInputString == secondInputString) {
         std::cout << "Match!" << std::endl;
+    }
 
+    std::string fileName;
+    std::cout << "Enter a filename in which to look for the string" << std::endl;
+    std::cin >> fileName;
+
+    std::ifstream fileInputStream(fileName);
+    std::string line;
+    if (fileInputStream.is_open()) {
+        std::cout << "Successfully opened " << fileName << std::endl;
+        while(true) {
+            if (fileInputStream.fail()) {
+                std::cout << "The file input stream went into an error state" << std::endl;
+                break;
+            }
+            getline(fileInputStream, line);
+            std::cout << line << std::endl;
+            if (firstInputString == line) {
+                std::cout << "Match!" << std::endl;
+            }
+        }
+    } else {
+        std::cout << "Could not open file for reading" << std::endl;
     }
 
     return 0;
